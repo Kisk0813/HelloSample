afe0fb12544fbdc4add7e927f779a179cbb88ee7
Keisuke <16010409@anabuki-college.ac.jp>
2018-05-24 10:59:08 +0900

    20180524 教科書P93追加編集                P97追加編集
    Signed-off-by: Keisuke <16010409@anabuki-college.ac.jp>

--- app/src/main/java/com/websarva/wings/android/hellosample/HelloSampleActivity.java
+++ app/src/main/java/com/websarva/wings/android/hellosample/HelloSampleActivity.java
@@ -3,6 +3,9 @@ package com.websarva.wings.android.hellosample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HelloSampleActivity extends AppCompatActivity {
 
@@ -10,6 +13,14 @@ public class HelloSampleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_sample);
+
+        // 表示ボタンであるButtonオブジェクトを取得
+        Button btClick = findViewById(R.id.btClick);
+        // リスナクラスのインスタンスを作成
+        HelloListener listener = new HelloListener();
+        // 表示ボタンにリスナを設定
+        btClick.sentOnClickListener(listener);
+        
     }
 
     /**
@@ -19,6 +30,12 @@ public class HelloSampleActivity extends AppCompatActivity {
 
         @Override
         public void onClick(View view){
+            // 名前入力欄であるEditTextオブジェクトを取得
+            EditText input = findViewById(R.id.etName);
+            //メッセージを表示するTextViewオブジェクトを取得
+            TextView output = findViewById(R.id.tvOutput);
+            //メッセージを表示
+            output.setText(inputStr + "さん、こんにちは！");
         }
     }
 }
