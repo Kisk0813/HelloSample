08fd128e47c4c92b0b2a247faade599b45240401
Keisuke <16010409@anabuki-college.ac.jp>
2018-05-24 10:46:41 +0900

    20180524 教科書P93追加編集

--- app/src/main/java/com/websarva/wings/android/hellosample/HelloSampleActivity.java
+++ app/src/main/java/com/websarva/wings/android/hellosample/HelloSampleActivity.java
@@ -2,6 +2,7 @@ package com.websarva.wings.android.hellosample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HelloSampleActivity extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class HelloSampleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_sample);
+    }
+
+    /**
+     * ボタンをクリックしたときのリスナクラス。
+     */
+    private class HelloListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view){
+        }
     }
 }
